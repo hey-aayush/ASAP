693356008c52032407e4bcf675bb9dd645a19e2e
Pradyumn Bhatter <55000494+prbhatter@users.noreply.github.com>
2022-04-23 00:47:13 +0530

    adding authentication

--- billify-server/src/server.js
+++ billify-server/src/server.js
@@ -1,7 +1,61 @@
 require('dotenv').config();
-const express = require('express');
 
+const express = require('express');
 const app = express();
+
+const mongoose = require("mongoose"); // mongo DB used to save users's account info
+const cors = require("cors");
+const passport = require("passport"); // passport-local used for user authentication
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const flash = require("express-flash");
+
+
+const dbURI = "mongodb://localhost/billify-database"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log("Mongoose Is Connected");
+    })
+    .catch(err => console.log(err));
+
+
+app.use(flash());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+////cors is used to allow cross-origin request
+
+app.use(
+  	cors({
+	    origin: "http://localhost:3000", 
+    	methods: [ "GET", "POST" ],
+    	credentials: true,
+  	})
+);
+
+app.use(
+    session({
+      	secret: "secretcode",
+      	resave: true,
+      	saveUninitialized: true,
+    })
+);
+
+app.use(cookieParser("secretcode"));
+
+////Initializing local-passport for user authentication
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport-config")(passport);
+
+const registerRoute = require('./routes/register')                  //Register route
+app.use(registerRoute)
+
+const loginRoute = require('./routes/login')                        //Login route
+app.use(loginRoute)
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
