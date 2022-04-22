508803a69cbfe32cde6c9b0f6b97668a30afa036
akstron <62210712+akstron@users.noreply.github.com>
2022-04-23 02:34:20 +0530

    products added

--- billify-server/src/middlewares/product.js
+++ billify-server/src/middlewares/product.js
@@ -1,5 +1,18 @@
 const Product = require('../models/Product');
 
 module.exports.AddProduct = async (req, res) => {
+    const {name, quantity, price} = req.body;
 
+    let product = await Product.findOne({
+        name, price
+    });
+
+    if(!product){
+        product = new Product({
+            name, price
+        });
+        await product.save();
+    }
+
+    
 }
